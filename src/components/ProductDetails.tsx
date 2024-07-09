8d73e15f1bb21de8b857735ccacfc5fe1bbb7c19
Eweh Victor <ewehvictor7@gmail.com>
2024-07-09 15:45:22 +0100

    Started product details

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,4 +1,14 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "../components/ui/breadcrumb";
+import { Tab, Tabs } from "./Tabs";
 
 export type CartProductType = {
   id: string;
@@ -14,6 +24,82 @@ const ProductDetails = () => {
   const { id } = useParams();
   return (
     <div>
+      <Breadcrumb className="text-primary2">
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link to="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link to={`/product/${id}`}>Product details</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <div>
+        <div>Image</div>
+        <div>Product info</div>
+      </div>
+
+      <div>
+        <Tabs>
+          <Tab label="Descriptions">
+            <div className="py-4">
+              <h2 className="text-lg font-medium mb-2">Descriptions</h2>
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                mollitia, molestiae quas vel sint commodi repudiandae
+                consequuntur voluptatum laborum numquam blanditiis harum
+                quisquam eius sed odit fugiat iusto fuga praesentium optio,
+                eaque rerum! Provident similique accusantium nemo autem.
+                Veritatis obcaecati tenetur iure eius earum ut molestias
+                architecto voluptate aliquam nihil, eveniet aliquid culpa
+                officia aut! Impedit sit sunt quaerat, odit, tenetur error,
+                harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia.
+                Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+                sapiente officiis modi at sunt excepturi expedita sint? Sed
+                quibusdam recusandae alias error harum maxime adipisci amet
+                laborum.
+              </p>
+            </div>
+          </Tab>
+          <Tab label="Additional information">
+            <div className="py-4">
+              <h2 className="text-lg font-medium mb-2">Tab 2 Content</h2>
+              <p className="text-gray-700">Tab 2 content</p>
+            </div>
+          </Tab>
+          <Tab label="Customer feedback">
+            <div className="py-4">
+              <h2 className="text-lg font-medium mb-2">Tab 3 Content</h2>
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                mollitia, molestiae quas vel sint commodi repudiandae
+                consequuntur voluptatum laborum numquam blanditiis harum
+                quisquam eius sed odit fugiat iusto fuga praesentium optio,
+                eaque rerum! Provident similique accusantium nemo autem.
+                Veritatis obcaecati tenetur iure eius earum ut molestias
+                architecto voluptate aliquam nihil, eveniet aliquid culpa
+                officia aut! Impedit sit sunt quaerat, odit, tenetur error,
+                harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia.
+                Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+                sapiente officiis modi at sunt excepturi expedita sint? Sed
+                quibusdam recusandae alias error harum maxime adipisci amet
+                laborum.
+              </p>
+            </div>
+          </Tab>
+        </Tabs>
+      </div>
+
       <h1>Product Details</h1>
       <p>Product ID: {id}</p>
       {/* Fetch and display more product details based on the ID */}
