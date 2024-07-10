dbca2d6a01e8dab53ce2c6f4df470264fc03d5ed
Eweh Victor <ewehvictor7@gmail.com>
2024-07-11 00:16:33 +0100

    Fixed success

--- src/components/paymentSuccess.tsx
+++ src/components/paymentSuccess.tsx
@@ -1,19 +1,47 @@
-import { FaRegCheckCircle } from "react-icons/fa";
 import Button from "../components/Button";
+import check from "../assets/check.svg";
 
 import { useNavigate } from "react-router-dom";
 
 const PaymentSuccess = () => {
   const navigate = useNavigate();
+
+  const date = new Date();
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  const formattedDate = date.toLocaleDateString("en-US", options);
+
   return (
-    <div className="absolute inset-0  top-0 flex flex-col items-center justify-center gap-2 bg-white bg-opacity-80">
+    <div className="absolute inset-0  top-0 flex flex-col items-center justify-center gap-2 bg-white z-10 font-poppins">
+      <p className="mt-4 text-xl font-semibold font-poppins text-[#747474]">
+        Thank You for Your Purchase!!🌟
+      </p>
+      <p className="mt-2 text-3xl font-semibold font-poppins">
+        Order placed Successfully
+      </p>
       <div className="z-10 text-center">
-        <FaRegCheckCircle className="text-9xl" />
+        <img src={check} className="" />
       </div>
-      <p className="mt-4 text-2xl font-semibold">Checkout Successful</p>
+
+      <div className=" border-t-2 p-4">
+        <h2 className="font-semibold">Order History</h2>
+        <div className="flex">
+          <p className="text-sm">ORDER NUMBER: </p>
+          <p className="text-sm">#002</p>
+        </div>
+        <div className="flex">
+          <p className="text-sm">DATE: </p>
+          <p className="text-sm">{formattedDate}</p>
+        </div>
+      </div>
+
       <div className="z-10 mt-6">
         <Button
-          label="View your orders"
+          label="Continue Shopping"
           onClick={() => {
             navigate("/");
           }}
